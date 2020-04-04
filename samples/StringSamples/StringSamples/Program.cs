3496faf34d4aae606e25cd614d4d8e3db7787882
Movasaghi <mmghho77@gmail.com>
2020-04-04 11:41:35 +0430

    change string

--- samples/StringSamples/StringSamples/Program.cs
+++ samples/StringSamples/StringSamples/Program.cs
@@ -10,6 +10,26 @@ namespace StringSamples
     {
         static void Main(string[] args)
         {
+            string a = "Hello,\nIam Movasaghinia.\nThis is my c# class.\nThis class is good for who want to learn c#.";
+            Console.WriteLine($"The main string:\n{a}");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++");
+            if (a.Contains("class"))
+            {
+                Console.WriteLine("It has \"class\"");
+            }
+            else
+            {
+                Console.WriteLine("There is no \"class\" in the string.");
+            }
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++");
+            string b = a.Replace("Hello", "Hi");
+            Console.WriteLine($"The b string:\n{b}");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++");
+            int index = a.IndexOf("class");
+            string c = a.Remove(index, ("class").Length);
+            Console.WriteLine($"The c string:\n{c}");
+
+            Console.ReadLine();
         }
     }
 }
