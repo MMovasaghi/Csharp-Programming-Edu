01ce05892ee144af097f57f1c9058d17121b7cd1
Movasaghi <mmghho77@gmail.com>
2020-04-06 19:37:00 +0430

    add list 1

---
+++ samples/lists/lists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>();
+
+            Console.Write("Number : ");
+
+            int length = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                a.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("+++++++++++++++++++++++++");
+            a.Add(10);
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
